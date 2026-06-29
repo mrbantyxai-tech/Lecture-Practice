2bc6b0966c2afe7fe06c4d762062989360f52a6f
Banty Prajapati <mrbantyxai@gmail.com>
2026-06-29 10:46:39 +0530

    Add files via upload

---
+++ JavaScript/29-06-2026/script.js
@@ -0,0 +1,73 @@
+/* Operators in Javr Script
+
+ 1. Arithmetic Operators 
+ */
+
+console.log("<<<<<<<<<<<<<< Arithmetic Operators >>>>>>>>>>>>>>");
+
+let r = 12; b1 = 21;
+
+console.log(r + b1);
+console.log(r - b1);
+console.log(r / b1);
+console.log(r * b1);
+console.log(r % b1);
+console.log(r ** b1);
+console.log(++r);
+console.log(++b1);
+console.log(b1++);
+console.log(b1);
+console.log(b1++);
+console.log(b1);
+
+//  2. Arithmetic Operators 
+
+console.log("<<<<<<<<<<<<<< Assignment Operators >>>>>>>>>>>>>>");
+
+let r1 = 12;
+
+console.log(r1 += 21);
+console.log(r1-= 21);
+console.log(r1 *= 21);
+console.log(r1 /= 21);
+console.log(r1 **= 21);
+console.log(r1 %= 21);
+
+//  3. Comparison Operators
+
+console.log("<<<<<<<<<<<<<< Comparison Operators >>>>>>>>>>>>>>");
+
+let r2 = 12; b2 = '12';
+
+console.log(r2 == b2);
+console.log(r2 === b2);
+console.log(r2 != b2);
+console.log(r2 !== b2);
+console.log(r2 > b2);
+console.log(r2 < b2);
+console.log(r2 >= b2);
+console.log(r2 <= b2);
+
+//  4. Logical Operators
+
+console.log("<<<<<<<<<<<<<< Logical Operators >>>>>>>>>>>>>>");
+
+let rfemale = true;
+let bmale = false;
+let goodheart = true;
+let badheart = false;
+
+console.log(rfemale && goodheart);
+console.log(bmale && badheart);
+console.log(rfemale || bmale);
+console.log(!rfemale);
+console.log(!bmale);
+
+
+
+
+
+
+
+
+
